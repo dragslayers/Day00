7826482be2dad5e3ebfd1b58102a322f5ff699e2
dragslayers <m.zhang@it-students.fr>
2021-09-13 12:41:58 +0200

    first commit

---
+++ exo02/bonjourV2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string x;
+
+    std::cout << "Bonjour, Quel est votre prénom\n";
+    std::cin >> x;
+    std::cout << x << ", enchanté moi c'est James, James Bond";
+
+return 0;
+
+}
+
+
