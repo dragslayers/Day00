2904ee0a3e96f06110ebe4418599452f56ce8755
admin <admin@iMac-de-admin.local>
2021-09-13 10:25:38 +0200

    exo00

---
+++ ex00/hitmachine2000.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+cout << "Moi je m'appelle 'Lolita'" << endl;
+cout << "Lo ou bien Lola, du pareil au même" << endl;
+cout << "Moi je m'appelle 'Lolita'" << endl;
+cout << "Quand je rêve aux Loups, c'est Lola qui saigne" << endl;
+cout << "[...]" << endl;
+cout << "C'est pas ma faute" << endl;
+cout << "Et quand je donne ma langue aux chats je vois les autres" << endl;
+cout << "Tout prêts à se jeter sur moi, c'est pas ma faute à moi" << endl;
+cout << "Si j'entends tout autour de moi" << endl;
+cout << "L.O.L.I.T.A, moi 'Lolita'" << endl;
+
+
+return 0;
+
+}
+
+
